f6b687a950153a220286d002d3e419859d10b809
KacperRutkowski <rutkowski.kacper.karol@gmail.com>
2025-11-13 05:55:28 +0100

    Edit docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-# Pobieramy oficjalny Tomcat 10 z JDK 21
-FROM tomcat:10.1.24-jdk21-temurin
+FROM eclipse-temurin:21-jdk
 
-# Usuń domyślny ROOT
-RUN rm -rf /usr/local/tomcat/webapps/ROOT
+WORKDIR /app
 
-# Skopiuj swój plik WAR do Tomcata
-COPY target/users_app.jar /usr/local/tomcat/webapps/ROOT.jar
+COPY target/users_app.war app.war
 
-# Ustawienie portu (Tomcat domyślnie 8080)
 EXPOSE 8080
 
-# Uruchom Tomcata w trybie foreground
-CMD ["catalina.sh", "run"]
+ENTRYPOINT ["java", "-jar", "app.war"]
--- scripts/build-and-push-backend.sh
+++ scripts/build-and-push-backend.sh
@@ -8,41 +8,29 @@ NC='\033[0m'
 DOCKER_REGISTRY="kacperroot"
 IMAGE_NAME="users-backend"
 
-echo -e "${GREEN}🔨 Build and push backendu${NC}"
+echo -e "${GREEN} Build and push backendu${NC}"
 echo ""
 
 # Build Maven
 echo "Maven build..."
+cd ..
 mvn clean package -DskipTests
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error during Maven build!${NC}"
-    exit 1
-fi
 
 # Tag z datą
 TAG=$(date +%Y%m%d-%H%M%S)
 FULL_IMAGE="${DOCKER_REGISTRY}/${IMAGE_NAME}:${TAG}"
 LATEST_IMAGE="${DOCKER_REGISTRY}/${IMAGE_NAME}:latest"
+export FULL_IMAGE
 
 # Build Docker
-echo "🐳 Docker build..."
-docker build -t ${FULL_IMAGE} .
+echo "Docker build..."
+docker compose -f docker-compose.yml build --no-cache
 docker tag ${FULL_IMAGE} ${LATEST_IMAGE}
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED} Error during Docker build!${NC}"
-    exit 1
-fi
 
 # Push
 echo "📤 Docker push..."
 docker push ${FULL_IMAGE}
 docker push ${LATEST_IMAGE}
 
-if [ $? -ne 0 ]; then
-    echo -e "${RED}❌ Error during push!${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}✅ Done!${NC}"
+echo -e "${GREEN}Done!${NC}"
 echo "Image: ${FULL_IMAGE}"
